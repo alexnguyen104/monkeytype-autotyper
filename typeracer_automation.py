96b7fdbf1ff2506e2d026bc14a0aab505128f061
unknown <alexnguyen104>
2022-12-01 19:18:00 +0700

    fixed bugs and errors

--- typeracer_automation.py
+++ typeracer_automation.py
@@ -4,48 +4,40 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-
-
-def get_words():
-    first_word = driver.find_element(
-        By.XPATH, '//*[@id="gwt-uid-20"]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td/div/div').text
-    # words = wait.until(EC.visibility_of_element_located(
-    #     (By.XPATH, '//*[@id="gwt-uid-20"]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td/div/div/span[3]'))).text
-    try:
-        words = driver.find_element(
-            By.XPATH, '//*[@id="gwt-uid-20"]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td/div/div/span[3]').text
-    except Exception:
-        words = driver.find_element(
-            By.XPATH, '//*[@id="gwt-uid-20"]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td/div/div/span[2]').text
-    return first_word + words
-
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
 
 def typing():
-    words = get_words()
+    words = driver.find_element(By.XPATH, '//*[@id="gwt-uid-20"]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr[1]/td/div/div').text
     input_section = driver.find_element(By.CLASS_NAME, "txtInput")
+
     for i in range(len(words)):
         for j in words[i]:
             input_section.send_keys(j)
-            if(i == len(words)/2 and j == "."):
+            if(i == len(words)-1 and j == "."):
                 return
-
             sleep(0.05)
 
-        input_section.send_keys(" ")
-
+# Settings
+options = Options()
+options.add_experimental_option("excludeSwitches", ["enable-logging"]) #disable logging messages
 
 driver = webdriver.Chrome(service=Service(
-    ChromeDriverManager().install()))
+    ChromeDriverManager().install()),options=options)
+
 driver.get("https://play.typeracer.com/")
 
-# wait = WebDriverWait(driver, 2)
+while True:
+    input("PRESS ENTER TO START...\n")
+    try:
+        typing()
+        break
+    except NoSuchElementException:
+        print("Enter a game to start")
+    except ElementNotInteractableException:
+        print("Wait for the game to start")
 
-input("PRESS ENTER TO START")
-typing()
-
-command = input("PRESS ENTER TO QUIT")
+input("PRESS ENTER TO QUIT...")
 
 driver.quit()
 sys.exit()
